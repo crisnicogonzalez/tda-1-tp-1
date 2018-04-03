23501a2510e3a89614f76659cdb92a293a3a8457
crisnicogonzalez <cristian3629@gmail.com>
2018-04-03 16:08:11 -0300

    Add quick sort

--- src/sort/quick_sort.py
+++ src/sort/quick_sort.py
@@ -1,39 +1,56 @@
 import logging
 import time
 
+from src.sort.selection_sort import switch
+
 FORMAT = "%(asctime)-15s    %(sort)-8s     %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO)
 log_info = {'sort': 'Quick Sort'}
 
 
-def select_pivote(list_to_sort):
-    [a_pos, b_pos, c_pos] = take_sample(list_to_sort)
-    logging.info('[{}, {}, {}]'.format(a_pos, b_pos,c_pos), extra=log_info)
-    a = list_to_sort[a_pos]
-    b = list_to_sort[b_pos]
-    c = list_to_sort[c_pos]
-    if a <= b and b >= c:
-        return b_pos
-    if b <= a and a >= c:
-        return a_pos
-    return c_pos
+def select_pivote(list_to_sort, first_position, last_position):
+    a = list_to_sort[first_position]
+    b = list_to_sort[first_position + (last_position - first_position) / 2]
+    c = list_to_sort[last_position]
+    media = a + b + c / 3
+    a_distance_media = abs(a-media)
+    b_distance_media = abs(b-media)
+    c_distance_media = abs(c-media)
+    if a_distance_media < b_distance_media and a_distance_media < c_distance_media:
+        return 0
+    if b_distance_media < a_distance_media and b_distance_media < c_distance_media:
+        return len(list_to_sort)/2
+    return len(list_to_sort)-1
 
 
-def take_sample(list_to_sort):
-    if len(list_to_sort) < 3:
-        return [0, 0, 0]
-    piece_len = len(list_to_sort) / 3
-    return list(map(lambda x: x*piece_len-1, range(1, 4)))
-
+def recursive_sort(list_to_sort, first_position, last_position):
+    pivote_position = select_pivote(list_to_sort, first_position, last_position)
+    pivote_value = list_to_sort[pivote_position]
+    i = first_position
+    j = last_position
+    switches = {}
+    while i < j:
+        i_advance = False
+        j_advance = False
+        if list_to_sort[i] > pivote_value:
+            i += 1
+            i_advance = True
+        if list_to_sort[j] < pivote_value:
+            j += 1
+            j_advance = True
+        if i_advance and j_advance:
+            switch(list_to_sort, i-1, j-1)
+            switches[i-1] = j-1
+            switches[j-1] = i-1
+    switch(switches[pivote_position, switches[pivote_position]])
+    recursive_sort(list_to_sort, first_position, i-1)
+    recursive_sort(list_to_sort, i+1, last_position)
 
 def sort(list_to_sort):
     start = time.time()
-    len_list = len(list_to_sort)
-    if len_list > 0:
-        pivote_position = select_pivote(list_to_sort)
+    recursive_sort(list_to_sort, 0, len(list_to_sort))
     end = time.time()
     logging.info('Tardo {} para {} elementos'.format(end-start, len_list), extra=log_info)
-
 
 
 
