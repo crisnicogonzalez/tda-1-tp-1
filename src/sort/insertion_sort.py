512fa75f069b2582ff162072bd8c6bc6296c4eab
crisnicogonzalez <cristian3629@gmail.com>
2018-04-03 00:02:16 -0300

    change loggin level

--- src/sort/insertion_sort.py
+++ src/sort/insertion_sort.py
@@ -12,16 +12,16 @@ def sort(list_to_sort):
     len_list = len(list_to_sort)
     for pivote in range(1, len_list):
         pivote_value = list_to_sort[pivote]
-        logging.info('La posicion pivote es {} y el valor es {}'.format(pivote, pivote_value), extra=log_info)
+        logging.debug('La posicion pivote es {} y el valor es {}'.format(pivote, pivote_value), extra=log_info)
         x = pivote - 1
         x_value = list_to_sort[x]
-        logging.info('X {} X value {}'.format(x, x_value), extra=log_info)
+        logging.debug('X {} X value {}'.format(x, x_value), extra=log_info)
         while pivote_value < x_value and x >= 0:
             x = x - 1
             x_value = list_to_sort[x]
-            logging.info('X {} X value {}'.format(x, x_value), extra=log_info)
+            logging.debug('X {} X value {}'.format(x, x_value), extra=log_info)
         if pivote_value >= x_value and x != pivote -1:
-            logging.info('insertar en la posicion {} el valor {}'.format(x+1, pivote_value), extra=log_info)
+            logging.debug('insertar en la posicion {} el valor {}'.format(x+1, pivote_value), extra=log_info)
             list_to_sort.insert(x+1, pivote_value)
             del list_to_sort[pivote+1]
         logging.info('La lista {}'.format(list_to_sort), extra=log_info)
