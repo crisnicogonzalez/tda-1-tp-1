1758f9e166b2423c2fbafb580a4a2442ed5e57b4
crisnicogonzalez <cristian3629@gmail.com>
2018-04-06 19:19:58 -0300

    fix merge_sort

--- src/sort/merge_sort.py
+++ src/sort/merge_sort.py
@@ -21,14 +21,14 @@ def sort_lists(first_list, second_list):
             list_to_return.append(j_value)
             j += 1
     if j == len(second_list):
-        return list_to_return.extend(first_list[i:])
+        list_to_return.extend(first_list[i:])
     else:
         list_to_return.extend(second_list[j:])
-        return list_to_return
+    return list_to_return
 
 
 def merge_sort(list_to_sort):
-    logging.info('List to sort {}'.format(list_to_sort), extra=logging)
+    logging.info('List to sort {}'.format(list_to_sort), extra=log_info)
     list_size = len(list_to_sort)
     if list_size <= 2:
         if list_size <= 1:
@@ -42,10 +42,14 @@ def merge_sort(list_to_sort):
         list_right = merge_sort(list_to_sort[:medium])
         list_left = merge_sort(list_to_sort[medium:])
         logging.info('Right list {} Left list {}'.format(list_right, list_left), extra=log_info)
-        return sort_lists(list_right, list_left)
+        list_sorted =  sort_lists(list_right, list_left)
+        logging.info('List sorted'.format(list_sorted), extra=log_info)
+        return list_sorted
 
 
 def sort(list_to_sort):
     start = time.time()
+    list_to_return = merge_sort(list_to_sort)
     end = time.time()
     logging.info('Tardo {} para {} elementos'.format(end-start, len(list_to_sort)), extra=log_info)
+    return list_to_return
