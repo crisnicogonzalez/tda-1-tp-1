8c3e62cb4d38c106999c6886b97e0b738679dee4
crisnicogonzalez <cristian3629@gmail.com>
2018-04-07 19:05:34 -0300

    Add run script and update to point d

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import logging
 import random
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 from src.sort.merge_sort import sort as merge_sort
 from src.sort.quick_sort import sort as quick_sort
 from src.sort.insertion_sort import sort as insertion_sort
@@ -49,22 +52,51 @@ def initialize_results_dict(set_lens):
     return results
 
 
+def calculate_mean_time_for_every_sort(results):
+    variance_and_mean_times = {}
+    for len_set in results:
+        variance_and_mean_times[len_set] = {}
+        for key_sort in results[len_set]:
+            execution_times = results[len_set][key_sort]
+            variance_and_mean_times[len_set][key_sort] = {
+                'mean': np.mean(execution_times),
+            }
+    return variance_and_mean_times
+
+
+def test_sorts(sets):
+    set_lens = [50, 100, 500, 1000, 2000, 3000, 4000, 5000, 7500, 10000]
+    results = initialize_results_dict(set_lens)
+    for key_sort in sorters:
+        sort = sorters[key_sort]
+        for set in sets:
+            for set_len in set_lens:
+                logging.info('Algoritmo {} para {} elementos'.format(key_sort, set_len), extra=log_info)
+                testing_set = set[:set_len]
+                ejecution_time = wrapper(testing_set, sort)
+                results[set_len][key_sort].append(ejecution_time)
+    return results
+
+
+def grafic_average():
+    plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+    plt.plot([1, 2, 3, 6], [1, 4, 9, 18], 'ro')
+    plt.axis([0, 6, 0, 20])
+    plt.show()
+
+
 def run():
     logging.info('Iniciando punto 1 item b del TP', extra=log_info)
     sets = generate_random_numbers_set()
     logging.info('Iniciando punto 1 item c del TP', extra=log_info)
-    set_lens = [50, 100, 500, 1000, 2000, 3000, 4000, 5000, 7500, 10000]
-    results = initialize_results_dict(set_lens)
-    for set_len in set_lens:
-        for set in sets:
-            testing_set = set[:set_len]
-            for key_sort in sorters:
-                sort = sorters[key_sort]
-                logging.info('Algoritmo {} para {} elementos'.format(key_sort, set_len), extra=log_info)
-                ejecution_time = wrapper(testing_set, sort)
-                results[set_len][key_sort].append(ejecution_time)
+    results = test_sorts(sets)
+    logging.info('Iniciando punto 1 item d del TP', extra=log_info)
+    averages_and_variances = calculate_mean_time_for_every_sort(results)
+    print averages_and_variances
+    grafic_average()
     print results
-    return True
+
+
 
 if __name__ == '__main__':
     run()
