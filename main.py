d59bbabb8e61b9689f6131bf480100c7ca885ac3
crisnicogonzalez <cristian3629@gmail.com>
2018-04-08 16:32:42 -0300

    Main refactor

--- main.py
+++ main.py
@@ -41,11 +41,10 @@ def print_result_of_sort(result):
 
 
 def print_sort_name_and_time(averages):
-    print averages
     for sort_name in averages:
         print sort_name
         for set_len in set_lens:
-            print('{},{}'.format(set_len, averages[sort_name][set_len]))
+            print('{0:10},{1:10f}'.format(set_len, averages[sort_name][set_len]))
 
 
 def calculate_mean_time_for_every_sort(results):
@@ -78,40 +77,46 @@ def test_sort(sets, sort):
     return results
 
 
+def run_merge_sort_wort_case():
+    logging.info('Merge sort', extra=log_info)
+    wort_set_for_merge_sort = generate_numbers_for_merge_sort_wort_case(10000)
+    normal_set_for_merge_sort = generate_random_numbers_set(10000, 10)
+    result_merge_sort_wort_case = test_sort(wort_set_for_merge_sort, merge_sort)
+    result_merge_sort_normal_case = test_sort(normal_set_for_merge_sort, merge_sort)
+    print_sort_name_and_time(calculate_mean_time_for_every_sort({'merge_sort_wort_case': result_merge_sort_wort_case}))
+    print_sort_name_and_time(calculate_mean_time_for_every_sort({'merge_sort_normal_case': result_merge_sort_normal_case}))
+
+
+def run_insertion_sort_wort_case():
+    logging.info('Insertion sort', extra=log_info)
+    wort_set_for_insertion_set = generate_numbers_in_orden(10000)
+    normal_set_for_insertion_set = generate_random_numbers_set(10000, 10)
+    result_insertion_sort_wort_case = test_sort(wort_set_for_insertion_set, insertion_sort)
+    result_insertion_sort_normal_case = test_sort(normal_set_for_insertion_set, insertion_sort)
+    print_sort_name_and_time(
+        calculate_mean_time_for_every_sort({'insertion_sort_wort_case': result_insertion_sort_wort_case}))
+    print_sort_name_and_time(
+        calculate_mean_time_for_every_sort({'insertion_sort_normal_case': result_insertion_sort_normal_case}))
+
+
 def run():
-    # logging.info('Iniciando punto 1 item b del TP', extra=log_info)
-    # sets = generate_random_numbers_set(10000, 10)
-    #
-    # logging.info('Iniciando punto 1 item c del TP', extra=log_info)
-    # results = test_sorts(sets)
-    #
-    # logging.info('Iniciando punto 1 item d del TP', extra=log_info)
-    # execution_time_average = calculate_mean_time_for_every_sort(results)
-    # print execution_time_average
-    # print_sort_name_and_time(execution_time_average)
-    #
-    # logging.info('Iniciando punto 1 item f del TP', extra=log_info)
-    # logging.info('Generando peor set para insertion sort', extra=log_info)
-    # wort_set_for_insertion_set = generate_numbers_in_orden(10000)
-    # result_insertion_sort = test_sort([wort_set_for_insertion_set], insertion_sort)
-    # wort_set_for_merge_sort = generate_numbers_for_merge_sort_wort_case(10000)
-    # normal_set_for_merge_sort = generate_random_numbers_set(10000, 10)
-    # result_merge_sort_wort_case = test_sort(wort_set_for_merge_sort, merge_sort)
-    # result_merge_sort_normal_case = test_sort(normal_set_for_merge_sort, merge_sort)
-    # print 'Merge sort wort case'
-    # print_sort_name_and_time(calculate_mean_time_for_every_sort({'merge_sort':result_merge_sort_wort_case}))
-    # print 'Merge sort normal case'
-    # print_sort_name_and_time(calculate_mean_time_for_every_sort({'merge_sort':result_merge_sort_normal_case}))
-    wort_set_for_quick_sort = generate_random_numbers_set()
-    normal_set_for_quick_sort = generate_random_numbers_set()
-    result_quick_sort_normal_case = test_sort(normal_set_for_quick_sort, quick_sort)
-    result_quick_sort_wort_case = test_sort(wort_set_for_quick_sort, naive_quick_sort)
-    print 'Quick sort normal case'
-    print_sort_name_and_time(calculate_mean_time_for_every_sort({'quick_sort': result_quick_sort_normal_case}))
-    print 'Quick sort wort case'
-    print_sort_name_and_time(calculate_mean_time_for_every_sort({'quick_sort': result_quick_sort_wort_case}))
-    # print result_insertion_sort
-    # print results
+    logging.info('Iniciando punto 1 item b del TP', extra=log_info)
+    sets = generate_random_numbers_set(10000, 10)
+
+    logging.info('Iniciando punto 1 item c del TP', extra=log_info)
+    results = test_sorts(sets)
+
+    logging.info('Iniciando punto 1 item d del TP', extra=log_info)
+    execution_time_average = calculate_mean_time_for_every_sort(results)
+    print_sort_name_and_time(execution_time_average)
+
+    logging.info('Iniciando punto 1 item f del TP', extra=log_info)
+    run_insertion_sort_wort_case()
+    run_merge_sort_wort_case()
+    
+
+
+
 
 
 if __name__ == '__main__':
