88131903a7c8cd47aed64731d503c5aa384188d9
crisnicogonzalez <cristian3629@gmail.com>
2018-04-08 22:06:52 -0300

    factory : file methods

--- src/algorithm/gale_shapley/gale_shapley_factory_set.py
+++ src/algorithm/gale_shapley/gale_shapley_factory_set.py
@@ -1,5 +1,9 @@
 from src.algorithm.gale_shapley.player import Player
 from src.algorithm.gale_shapley.team import Team
+import os
+
+
+path_files = 'src/files/gale_shapley/'
 
 
 def factory(players_name, teams_name):
@@ -8,11 +12,12 @@ def factory(players_name, teams_name):
     teams_draft = up_draft(teams_name, 'equipo')
     relationate(players_draft, players)
     relationate(teams_draft, teams)
+    return players, teams
 
 
 def relationate(draft, instances_dict, other_instances_dict):
     for instance_name in draft:
-        draft_instance = draft_dict[instance_name]
+        draft_instance = draft[instance_name]
         instance = instances_dict[instance_name]
         for position in range(0, len(draft_instance)):
             instance.add(other_instances_dict[draft_instance[position]],position)
@@ -29,22 +34,25 @@ def create_instances(players_name, teams_name):
 
 
 def exist_file_by_name(file_name):
-    return True
+    return os.path.isfile(file_name)
 
 
 def up_file_by_name(file_name):
-    return False
+    return open(file_name, 'r')
 
 
 def format_file(file):
-    return []
+    draft = []
+    for line in file:
+        draft.append(int(line))
+    return draft
 
 
 def up_draft(names, key_word_file):
     draft = {}
     for name in names:
         file_name = '{}_[{}]'.format(key_word_file, players_name)
-        if exist_file_by_name(file_name):
-            file = up_file_by_name(file_name)
+        if exist_file_by_name(path_files+file_name):
+            file = up_file_by_name(path_files+file_name)
             draft[name] = format_file(file)
     return draft
